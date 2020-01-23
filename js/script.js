4b1672344c54fbc615548207774665f7a39b8970
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-23 22:46:18 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+  $("#footer-chat input").keypress(function(event) {
+    if (event.which == 13) {
+      var chatTemplate = $("main-chat .user-message-template").clone();
+      chatTemplate.children("p").text($(this).val());
+      $("main-chat").append(chatTemplate);
+    }
+  });
 });
